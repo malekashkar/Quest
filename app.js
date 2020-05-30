e3a62cb0a7a31a145b405b87ede8f03e9b54bed6
Dep0sit <62209591+Dep0sit@users.noreply.github.com>
2020-05-30 09:15:46 -0400

    Base of the bot

---
+++ app.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const mongoose = require("mongoose");
+const client = new Discord.Client();
+const Enmap = require('enmap');
+const fs = require('fs');
+
+client.config = config;
+client.commands = new Enmap();
+
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, props);
+  });
+});
+
+client.login(config.token);
